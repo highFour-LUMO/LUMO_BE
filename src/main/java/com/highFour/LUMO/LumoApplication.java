ecf374f80d768d914cbd5714a7bef6fa9b249977
suhyunpark <psh7492@gmail.com>
2025-02-04 20:28:22 +0900

    chore: 프로젝트 구조 세팅

---
+++ src/main/java/com/highFour/LUMO/LumoApplication.java
@@ -0,0 +1,13 @@
+package com.highFour.LUMO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/highFour/LUMO/LumoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highFour.LUMO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
