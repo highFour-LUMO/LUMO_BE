b6706fed432b2c7fe1dce9ed8f0b9b65a3f4d324
suhyunpark <psh7492@gmail.com>
2025-02-10 14:29:23 +0900

    chore: BaseTimeEntity 세팅

---
+++ src/main/java/com/highFour/LUMO/common/domain/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package com.highFour.LUMO.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+	@Column(updatable = false)
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+
+	private LocalDateTime deletedAt;
+
+}
