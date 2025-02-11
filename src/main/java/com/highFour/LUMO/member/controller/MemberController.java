6939efdafa456e16005da904fd5dd431aeb2665f
snikuz12 <snikuz12@naver.com>
2025-02-11 16:29:12 +0900

    feat : memberExceptionType, api ~ing

--- src/main/java/com/highFour/LUMO/member/controller/MemberController.java
+++ src/main/java/com/highFour/LUMO/member/controller/MemberController.java
@@ -1,21 +1,34 @@
-//package com.highFour.Hand2Hand.member.controller;
-//
-//
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/member")
-//@Slf4j
-//public class MemberController {
-//
-//    @PostMapping("/sign-up")
-//    public ResponseEntity<?> signUp() {
-//
-//    };
-//}
+package com.highFour.Hand2Hand.member.controller;
+
+
+import com.highFour.LUMO.member.dto.MemberCreateReqDto;
+import com.highFour.LUMO.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/member")
+@Slf4j
+public class MemberController {
+    private final MemberService memberService;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody MemberCreateReqDto createReqDto) {
+        try {
+            memberService.register(createReqDto);
+            return ResponseEntity.ok(new CommonResDto(HttpStatus.OK, "회원가입 성공", null));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new CommonResDto(HttpStatus.BAD_REQUEST, "회원가입에 실패했습니다: " + e.getMessage(), null));
+        }
+    }
+}
--- src/main/java/com/highFour/LUMO/member/service/MemberService.java
+++ src/main/java/com/highFour/LUMO/member/service/MemberService.java
@@ -1,4 +1,36 @@
 package com.highFour.LUMO.member.service;
 
+import com.highFour.LUMO.member.dto.MemberCreateReqDto;
+import com.highFour.LUMO.member.entity.Member;
+import com.highFour.LUMO.member.repository.MemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
+        this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    private void validateRegistration(MemberCreateReqDto createReqDto) {
+        if (createReqDto.password().length() <= 7) {
+            throw new RuntimeException("비밀번호는 8자 이상이어야 합니다.");
+        }
+
+        if (memberRepository.existsByEmail(createReqDto.email())  ) {
+            throw new RuntimeException("이미 사용중인 이메일 입니다.");
+        }
+
+    }
+    public void register(MemberCreateReqDto createReqDto) {
+        validateRegistration(createReqDto);
+
+        Member member = createReqDto.toEntity(passwordEncoder.encode(createReqDto.password()));
+        memberRepository.save(member);
+    }
 }
