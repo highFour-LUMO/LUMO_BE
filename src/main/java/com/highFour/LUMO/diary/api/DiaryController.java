d2339bc01b07e56bfc2d1f7c752640748f69ef72
suhyunpark <psh7492@gmail.com>
2025-03-10 20:03:44 +0900

    fix: Response 201에서 200로 변경

--- src/main/java/com/highFour/LUMO/diary/api/DiaryController.java
+++ src/main/java/com/highFour/LUMO/diary/api/DiaryController.java
@@ -37,56 +37,56 @@ public class DiaryController {
 	@PostMapping("")
 	public ResponseEntity<?> createDiary(@RequestBody DiaryCreateReq reqDto) {
 		Diary diary = diaryService.createDiary(reqDto);
-		return new ResponseEntity<>(diary, HttpStatus.CREATED);
+		return new ResponseEntity<>(diary, HttpStatus.OK);
 	}
 
 	// 일기 상세 조회
 	@GetMapping("/{diaryId}")
 	public ResponseEntity<?> getDiaryDetail(@PathVariable(name = "diaryId") Long diaryId) {
 		DiaryDetRes diary = diaryService.getDiaryByDiaryId(diaryId);
-		return new ResponseEntity<>(diary, HttpStatus.CREATED);
+		return new ResponseEntity<>(diary, HttpStatus.OK);
 	}
 
 	// 	일기 목록 조회
 	@GetMapping("")
 	public ResponseEntity<?> getDiaryListByType(@RequestParam DiaryType type) {
 		List<DiaryListRes> diaryList = diaryService.getDiaryList(type);
-		return new ResponseEntity<>(diaryList, HttpStatus.CREATED);
+		return new ResponseEntity<>(diaryList, HttpStatus.OK);
 	}
 
 	// 일기 삭제
 	@DeleteMapping("/{diaryId}")
 	public ResponseEntity<?>  deleteDiary(@PathVariable(name = "diaryId") Long diaryId) {
 		 diaryService.deleteDiary(diaryId);
-		return new ResponseEntity<>(HttpStatus.CREATED);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 	// 일기 삭제
 	@PatchMapping("")
 	public ResponseEntity<?> updateDiary(@RequestBody DiaryUpdateReq reqDto) {
 		diaryService.updateDiary(reqDto);
-		return new ResponseEntity<>("일기/감사일기 수정 성공", HttpStatus.CREATED);
+		return new ResponseEntity<>("일기/감사일기 수정 성공", HttpStatus.OK);
 	}
 
 	// 검색어로 검색
 	@GetMapping("/search")
 	public ResponseEntity<?>  searchByKeyword(@RequestBody DiarySearchReq dto) {
 		List<DiaryListRes> diaryList = diaryService.searchByKeyword(dto);
-		return new ResponseEntity<>(diaryList, HttpStatus.CREATED);
+		return new ResponseEntity<>(diaryList, HttpStatus.OK);
 	}
 
 	// 주간 평균 점수
 	@GetMapping("/rating/weekly")
 	public ResponseEntity<?>  getWeeklyAvgRating() {
 		Double weeklyAvgRating  = diaryService.getWeeklyAvgRating();
-		return new ResponseEntity<>(weeklyAvgRating, HttpStatus.CREATED);
+		return new ResponseEntity<>(weeklyAvgRating, HttpStatus.OK);
 	}
 
 	// 월간 평균 점수
 	@GetMapping("/rating/monthly")
 	public ResponseEntity<?>  getMonthlyAvgRating() {
 		Double weeklyAvgRating  = diaryService.getMonthlyAvgRating();
-		return new ResponseEntity<>(weeklyAvgRating, HttpStatus.CREATED);
+		return new ResponseEntity<>(weeklyAvgRating, HttpStatus.OK);
 	}
 
 
