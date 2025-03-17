b87ea1c8bddc7385c374e7bf1a2de2b5d1f42fc7
tteia <tteia.dl@gmail.com>
2025-03-17 16:32:30 +0900

    feat: 친구 요청 수락 / 거절 기능 구현

--- src/main/java/com/highFour/LUMO/common/exceptionType/FriendExceptionType.java
+++ src/main/java/com/highFour/LUMO/common/exceptionType/FriendExceptionType.java
@@ -8,7 +8,9 @@ public enum FriendExceptionType implements ExceptionType {
 
     FRIEND_NOT_FOUND(HttpStatus.NOT_FOUND, "등록된 친구가 없습니다."),
     ALREADY_REQUESTED(HttpStatus.BAD_REQUEST, "해당 친구와의 요청이 존재합니다."),
-    ALREADY_FRIENDS(HttpStatus.BAD_REQUEST, "이미 상대와 친구 상태입니다.");
+    ALREADY_FRIENDS(HttpStatus.BAD_REQUEST, "이미 상대와 친구 상태입니다."),
+    REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND,"해당 친구 요청을 찾을 수 없습니다."),
+    INVALID_REQUEST_STATUS(HttpStatus.BAD_REQUEST,"유효하지 않은 친구 요청 상태입니다.");
 
 
 
--- src/main/java/com/highFour/LUMO/friend/repository/FriendRequestRepository.java
+++ src/main/java/com/highFour/LUMO/friend/repository/FriendRequestRepository.java
@@ -1,10 +1,12 @@
 package com.highFour.LUMO.friend.repository;
 
-import com.highFour.LUMO.friend.entity.Friend;
 import com.highFour.LUMO.friend.entity.FriendRequest;
 import com.highFour.LUMO.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
     boolean existsBySenderAndReceiver(Member sender, Member receiver);
+    Optional<FriendRequest> findBySenderAndReceiver(Member sender, Member receiver);
 }
